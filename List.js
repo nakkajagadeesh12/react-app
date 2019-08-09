65825ab12f7fe0d712ced6488da7c4ece40a4dc8
nakkajagadeesh12 <nakkajagadeesh1234@gmail.com>
2019-08-09 12:59:09 +0530

    3 files modified

--- List.js
+++ List.js
@@ -4,8 +4,25 @@ import './style1.scss';
 class List extends React.Component {
   render() {
     return (
-      <div style={{backgroundColor:"cyan"}}>
-        second section
+      <div class="fluid-container main" style={{ backgroundColor: "cyan", padding: "10px" }}>
+        <div class="container row div" style={{ color: "white", padding: "10px" }}>
+          <div class="col-lg-2">
+            <div class="dropdown">
+              <button type="select" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+                Menswear
+  </button>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">Link 1</a>
+                <a class="dropdown-item" href="#">Link 2</a>
+                <a class="dropdown-item" href="#">Link 3</a>
+              </div>
+            </div>
+
+          </div>
+          <div class="col-lg-2">Womenswear</div>
+          <div class="col-lg-2">Kidswear</div>
+          <div class="col-lg-2">Electronics</div>
+        </div>
       </div>
     )
   }
