9d24d6ddded2fcdc1cce433ffdd219812e667dd1
nakkajagadeesh12 <nakkajagadeesh1234@gmail.com>
2019-08-21 16:22:27 +0530

    1 file modified

--- ItemList.js
+++ ItemList.js
@@ -1,41 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './ItemList.css';
 
-class ItemList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: '',
-    }
-  }
+// class ItemList extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       data: '',
+//     }
+//   }
+//   componentWillMount() {
+//     fetch('https://jsonplaceholder.typicode.com/photos').then((response) => response.json()).then((response) => this.setState({ data: response }));
+//   }
+//   render() {
+//     const { data } = this.state;
+//     return (
+//       <div className="item-list">
+//         {data && data.map(item => {
+//           return (
+//             <div className="item-it">
+//               <div className="it-1">
+//                 <img src={item.thumbnailUrl} alt="image" />
+//                 <div className="it-3">
+//                   <p>product {item.id}</p>
+//                 </div>
+//               </div>
+//               <div className="it-2">
+//                 <div className="class1"><button className="btn1">Add to cart</button></div>
+//                 <div className="class2"><button className="btn2">Buy Now</button></div>
+//               </div>
 
-  componentWillMount() {
-    fetch('https://jsonplaceholder.typicode.com/photos').then((response) => response.json()).then((response) => this.setState({ data: response }));
-  }
+//             </div>)
+//         })}
+//       </div>
+//     )
+//   }
+// }
 
-  render() {
-    const { data } = this.state;
-    return (
-      <div className="item-list">
-        {data && data.map(item => {
-          return (
-            <div className="item-it">
-              <div className="it-1">
-                <img src={item.thumbnailUrl} alt="image" />
-                <div className="it-3">
-                  <p>product {item.id}</p>
-                </div>
+const ItemList = () => {
+  const [data, setData] = useState(0);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/photos').then((response) => response.json()).then((response) => setData(response))
+  }, [])
+  return (
+    <div className="item-list">
+      {data && data.map(item => {
+        return (
+          <div className="item-it">
+            <div className="it-1">
+              <img src={item.thumbnailUrl} alt="image" />
+              <div className="it-3">
+                <p>product {item.id}</p>
               </div>
-              <div className="it-2">
-                <div className="class1"><button className="btn1">Add to cart</button></div>
-                <div className="class2"><button className="btn2">Buy Now</button></div>
-              </div>
+            </div>
+            <div className="it-2">
+              <div className="class1"><button className="btn1">Add to cart</button></div>
+              <div className="class2"><button className="btn2">Buy Now</button></div>
+            </div>
 
-            </div>)
-        })}
-      </div>
-    )
-  }
+          </div>)
+      })}
+    </div>
+  )
 }
 
 export default ItemList;
