5e5b2e51a8951519c7fb77e074cc3e8e5a69c819
nakkajagadeesh12 <nakkajagadeesh1234@gmail.com>
2019-08-21 18:33:42 +0530

    2 files modified

--- List.js
+++ List.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import './style1.scss';
 
+const list = ['Menswear', 'Womenswear', 'Kidswear', 'Electronics', 'Furniture'];
+
 class List extends React.Component {
   render() {
     return (
       <div class="fluid-container main menu-styl" >
         <div class="container row div" style={{ color: "white", padding: "10px" }}>
-          <div class="col-lg-2 item">
-          Menswear
-          <div className="dropdown-content">
-          <a href="#">link1</a>
-          <a href="#">link2</a>
-          <a href="#">link3</a>
-          <a href="#">link4</a>
-          </div>
-          </div>
-          <div class="col-lg-2 item">Womenswear</div>
-          <div class="col-lg-2 item">Kidswear</div>
-          <div class="col-lg-2 item">Electronics</div>
-          <div class="col-lg-2 item">Furniture</div> 
+          {
+            list && list.map((item) => {
+              return (
+                <div class="col-lg-2 item">
+                  <div className="dropdown">
+                    {item}
+                    <div className="dropdown-content">
+                      <a href="#">link1</a>
+                      <a href="#">link2</a>
+                      <a href="#">link3</a>
+                      <a href="#">link4</a>
+                    </div>
+                  </div>
+                </div>
+              )
+            })
+          }
+          <div class="col-lg-2 cart-icon"><i class="fas fa-shopping-cart"></i></div>
         </div>
       </div>
     )
