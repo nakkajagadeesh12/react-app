bd7a96b95d1239a832016cff6efc4a87e559d5eb
nakkajagadeesh12 <nakkajagadeesh1234@gmail.com>
2019-08-21 15:51:58 +0530

    1 file modified

--- ItemList.js
+++ ItemList.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './ItemList.css';
 
-const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 class ItemList extends React.Component {
   constructor(props) {
     super(props);
