a580e5a381ab2cf4cefe74895516247f6ddb8d2d
nakkajagadeesh12 <nakkajagadeesh1234@gmail.com>
2019-08-22 10:34:56 +0530

    4 files modified

--- Login.js
+++ Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hello from './Hello';
 import List from './List';
+import './Login.css';
 
 const Login = () => {
   return (
@@ -8,6 +9,23 @@ const Login = () => {
       <Hello />
       <List />
       <h1>Login</h1>
+      <div class="jumbotron login-class">
+        <form>
+          <div>
+            <label for="uname">UserName: </label><br />
+            <input type="text" required id="uname" name="uname" />
+          </div>
+          <br />
+          <div>
+            <label for="pwd">Password: </label><br />
+            <input type="password" required id="pwd" name="pwd" />
+          </div>
+          <br />
+          <div class="login-btn">
+            <button class="btn btn-primary log-btn">Login</button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
--- Signup.js
+++ Signup.js
@@ -12,26 +12,26 @@ const Signup = () => {
       <div class="jumbotron form-class">
         <form>
           <div>
-            <label for="uname">UserName: </label>
+            <label for="uname">UserName: </label> <br />
             <input type="text" required id="uname" name="uname" />
           </div>
           <br />
           <div>
-            <label for="email">Email: </label>
+            <label for="email">Email: </label> <br />
             <input type="email" required id="email" name="email" />
           </div>
           <br />
           <div>
-            <label for="pno">PhoneNo: </label>
+            <label for="pno">PhoneNo: </label> <br />
             <input type="text" required id="pno" name="pno" />
           </div>
           <br />
           <div>
-            <label for="pwd">Password: </label>
+            <label for="pwd">Password: </label> <br />
             <input type="password" required id="pwd" name="pwd" />
           </div>
           <br />
-          <div>
+          <div className="signin-btn">
             <button class="btn btn-primary">Register</button>
           </div>
         </form>
