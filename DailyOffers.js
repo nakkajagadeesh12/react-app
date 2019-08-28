2e46c7cea26dff7685b013d93d59165d8d067f01
nakkajagadeesh12 <nakkajagadeesh1234@gmail.com>
2019-08-28 19:47:32 +0530

    1 file modified

--- DailyOffers.js
+++ DailyOffers.js
@@ -3,6 +3,9 @@ import './DailyOffers.css';
 
 
 const DailyOffers=()=>{
-  return <h5 className='offer-zone'><marquee>Today Offers</marquee></h5>
+  return <div><h5 className='offer-zone'>
+  <marquee>Today Offers</marquee>
+  <img src={'https://cdn.pixabay.com/photo/2014/10/04/15/31/sticker-473635_960_720.png'} />
+  </h5></div>
 }
 export default DailyOffers;
